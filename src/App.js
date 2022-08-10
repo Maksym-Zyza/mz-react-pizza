0a11b6aa943900b6ac8e0855d5dc1e70cd13c328
Maksym Zyza <maksym.zyza@telus.com>
2022-08-10 13:29:17 +0300

    update(attributes): updated attributes in react components, config eslint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import './App.css';
 import './scss/app.scss';
 import { Header } from './components/Header/Header';
@@ -9,16 +9,16 @@ import { PizzaBlock } from './components/PizzaBlock/PizzaBlock';
 const App = () => {
   return (
     <div className="App">
-      <div class="wrapper">
+      <div className="wrapper">
         <Header />
-        <div class="content">
-          <div class="container">
-            <div class="content__top">
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
               <Categories />
               <Sort />
             </div>
-            <h2 class="content__title">Все пиццы</h2>
-            <div class="content__items">
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
               <PizzaBlock title="piza1" prise="15" />
               <PizzaBlock title="piza2" prise="20" />
               <PizzaBlock />
