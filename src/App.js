fbbf986e8dae9ad2970c01062110f06de45a5b84
Maksym Zyza <maksym.zyza@telus.com>
2022-08-14 10:35:03 +0300

    feat(fetchData): added fetching data from api service (mockapi.io) - v6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import './scss/app.scss';
-import pizzas from './pizzas.json';
 import { Header } from './components/Header/Header';
 import { Categories } from './components/Categories/Categories';
 import { Sort } from './components/Sort/Sort';
 import { PizzaBlock } from './components/PizzaBlock/PizzaBlock';
 
 const App = () => {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch('https://62f897b43eab3503d1d855b0.mockapi.io/api/items')
+      .then(response => response.json())
+      .then(result => setPizzas(result))
+      .then(error => console.log(error));
+  }, []);
+
   return (
     <div className="App">
       <div className="wrapper">
